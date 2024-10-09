b8af0a05950e84c932e23a520b0d5acacbb39ca9
david <mangasaryan@gmail.com>
2024-10-09 18:08:05 +0300

    commit with auth

--- Monica/settings.py
+++ Monica/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-a = 456
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profileUser'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Monica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dl',
+        'USER': 'root',
+        'PASSWORD': 'davit',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
